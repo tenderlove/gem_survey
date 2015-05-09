504b88038119a5a74ee338474548f2f7416d37bc
Brandon Weaver <keystonelemur@gmail.com>
2015-05-09 00:23:48 -0700

    cleanup

--- survey_all.rb
+++ survey_all.rb
@@ -1,10 +1,13 @@
 `curl https://raw.githubusercontent.com/tenderlove/gem_survey/master/survey.rb | ruby`
 
-ruby_project_paths = r['**/Gemfile'].map { |s| s.split('/')[0..-2].join('/') }
+ruby_project_paths = Dir['/**/Gemfile'].map { |s| s.split('/')[0..-2].join('/') }
 
 ruby_project_paths.each { |path|
+  puts "Running in #{path}"
+
   Dir.chdir(path)
   `curl https://raw.githubusercontent.com/tenderlove/gem_survey/master/survey.rb | bundle exec ruby`
+  puts "Cleaning"
   File.delete('survey.rb')
 }
 
